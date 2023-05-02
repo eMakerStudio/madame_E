7241765c4d6d498221eba50d6c3634b4ddaac6cb
Barbara Hohensee <emaker.studio@gmail.com>
2023-05-02 09:13:12 +0200

    erster Version

---
+++ zoo-futter-automat.py
@@ -0,0 +1,20 @@
+'''Schreibe ein Programm für einen Futterautomaten im Tierpark.
+   Es gibt zwei Sorten Futter: Möhren (sind für Pferde, Esel, Kaninchen)
+   und Nüsse (sind für Kaninchen, Vögel, Schildkröten)'''
+
+nuesse = "1"
+moehren = "2"
+
+print("Zoo-Futter-Automat")
+
+''' Erstelle das Menü '''
+
+''' Prüfe welches Futter ausgewählt wurde '''
+
+''' Gebe das ausgewählte Futter auf dem Bildschirm aus '''
+
+''' Gebe Hinweise für die Fütterung der Tiere '''
+
+''' Verabschiede den Kunden '''
+
+''' Begrüße den nächsten Kunden '''
